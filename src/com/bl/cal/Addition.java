14dab7beb81bd2cc752d3e41c4acaac2e1b34e88
lunainfotec <kirtimadan1997@gmail.com>
2023-04-14 12:52:49 +0530

    added : multiply of number

--- src/com/bl/cal/Addition.java
+++ src/com/bl/cal/Addition.java
@@ -5,9 +5,9 @@ public class Addition {
 
         int a =10;
         int b =5;
-        int c = a+b;
+        int c = a*b;
 
-        System.out.println("Addition :" + c);
+        System.out.println("multiply :" + c);
 
     }
 }
